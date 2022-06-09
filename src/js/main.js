0b71deec8a7e16d654210b35b0cf244c5a0c7e1a
Thiago <thiagoamartins0@gmail.com>
2022-06-09 16:06:33 -0300

    eliminando código repetido

--- src/js/main.js
+++ src/js/main.js
@@ -1,39 +1,23 @@
 const boxProdutos = document.querySelector("#boxProducts");
+const buttonMoreProducts = document.getElementById("buttonMoreProducts");
+
+let products = "";
 
 const xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
         const response = JSON.parse(this.responseText);
+
+        listOfProducts(response);
         
-        let products = "";
-
-        response.products.forEach(product => {
-            products += `
-            <div class="product">
-                <div class="image">
-                    <img src="${product.image}" alt="${product.name}" />
-                </div>
-                
-                <div class="product-information">
-                    <h4 class="title-product">${product.name}</h4>
-
-                    <p class="description">
-                        ${product.description}
-                    </p>
-
-                    <span class="old-price">De: R$ ${product.oldPrice},00</span>
-
-                    <span class="new-price">Por: R$ ${product.price},00<span>ou ${product.installments.count}x de R$ ${product.installments.value}</span></span>
-
-                    <button class="button">Comprar</button>            
-                </div>                 
-            </div>
-        `;
-            console.log(`${product.installments.count}`)
-        });
-
         boxProdutos.innerHTML = products;
+
+        buttonMoreProducts.addEventListener("click", () => {
+            listOfProducts(response);
+            
+            boxProdutos.innerHTML = products;
+        });
     }
 };
 
@@ -41,3 +25,33 @@ xhttp.open("GET", "https://frontend-intern-challenge-api.iurykrieger.vercel.app/
 
 xhttp.send();
 
+function listOfProducts(response) {
+    response.products.forEach(product => {
+        products += `
+            <div class="product">
+                <div class="image">
+                    <img src="${product.image}" alt="${product.name}" />
+                </div>
+                
+                <div class="product-information">
+                    <h4 class="title-product">${product.name}</h4>
+    
+                    <p class="description">
+                        ${product.description}
+                    </p>
+    
+                    <span class="old-price">De: R$ ${product.oldPrice},00</span>
+    
+                    <span class="new-price">Por: R$ ${product.price},00<span>ou ${product.installments.count}x de R$ ${product.installments.value}</span></span>
+    
+                    <button class="button">Comprar</button>            
+                </div>                 
+            </div>
+        `;
+    });
+}
+
+/**
+ * O que tenho que fazer?
+ * - evitar código repetido utilizando funções
+ */
